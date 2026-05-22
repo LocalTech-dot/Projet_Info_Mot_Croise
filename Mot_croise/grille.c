f8d22254c10d5072221c3e3f77582b28dd44b5bc
Max Vaillant <maxvaillant@MacBook-de-Max.local>
2026-05-22 15:58:10 +0200

    avancement sur la grille

--- Mot_croise/grille.c
+++ Mot_croise/grille.c
@@ -4,10 +4,17 @@
 #include "grille.h"
 #include "header.h"
 
-void initialiser_grille(int lignes, int colonnes, char plateau[16][16]) {
-    for (int i = 0; i < lignes; i++) {
-        for (int j = 0; j < colonnes; j++) {
-            plateau[i][j] = ' ';
+void placer_mot(char plateau[16][16], char* mot, int x_depart, int y_depart, int dx, int dy, int nb_lignes, int nb_colonnes) {
+
+    int longeur = strlen(mot);
+
+    for (int i = 0; i < longeur; ++i) {
+        int pos_x = x_depart + (i*dx);
+        int pos_y = y_depart + (i*dy);
+        if (pos_x < 0 || pos_x >= nb_colonnes || pos_y < 0 || pos_y >= nb_lignes) {
+            return 0;
         }
     }
 }
+
+
